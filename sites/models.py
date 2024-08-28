a147f1c43e4c121597b6973fc6fff075fc3dd95f
Programmer5 <u1ablumenthalvarnado4@gmail.com>
2024-08-28 16:37:36 +0300

    modified sites and user apps

---
+++ sites/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+
+class Site(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        unique_together = ('user', 'name',)
+        # This ensures that each user can only have one unique name for a site
---
+++ user/views.py
@@ -0,0 +1,46 @@
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.urls import reverse_lazy
+
+from .forms import SignUpForm, EditProfileForm
+from django.contrib.auth.decorators import login_required
+
+
+@login_required  # Ensure only logged-in users can access this view
+def user_profile(request):
+    user = request.user
+    context = {
+        'email': user.email,
+        'username': user.username
+    }
+    return render(request, 'user/user_profile.html', context)
+
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return HttpResponseRedirect(reverse_lazy("user:profile"))
+    else:
+        form = SignUpForm()
+    return render(request, 'user/signup_template.html', {'form': form})
+
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return HttpResponseRedirect(reverse_lazy("user:profile"))
+    else:
+        form = EditProfileForm(instance=request.user)
+    return render(request, 'user/edit_profile.html', {'form': form})
