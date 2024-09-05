a0ed695b98d35dd6f48a99fa7f7e9613159a212f
Programmer5 <u1ablumenthalvarnado4@gmail.com>
2024-09-05 13:46:24 +0300

    fix for not found pages

--- VPN/views.py
+++ VPN/views.py
@@ -4,7 +4,7 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseNotFound
 
 
 from VPN.utils import (format_media_link, update_used_traffic,
@@ -53,7 +53,6 @@ def proxy_view(request, site_name, path=None):
         if log["method"] == "network.responsereceived"
     ])
     update_used_traffic(traffic_amount=total_traffic, user_site=user_site)
-
     update_transitions_count(user_site=user_site)
     soup = change_soup_links(soup=soup,
                              base_url=base_url,
@@ -91,45 +90,48 @@ def static_files_proxy_view(request, site_name, resource_path):
             "Chrome/128.0.0.0 Safari/537.36"
         )
     }
-    response = requests.get(resource_path, headers=headers)
-    response.raise_for_status()
+    try:
+        response = requests.get(resource_path, headers=headers)
+        response.raise_for_status()
 
-    # Determine the content type and host for proxying URLs
-    content_type = response.headers.get('content-type')
-    current_host = f"{request.scheme}://{request.get_host()}/proxy"
+        # Determine the content type and host for proxying URLs
+        content_type = response.headers.get('content-type')
+        current_host = f"{request.scheme}://{request.get_host()}/proxy"
 
-    if content_type and content_type.startswith('text/css'):
-        content = response.text
+        if content_type and content_type.startswith('text/css'):
+            content = response.text
 
-        # Modify URLs in CSS
-        url_pattern = re.compile(r'url\((\/[^)]+)\)')
-        matches = url_pattern.findall(content)
+            # Modify URLs in CSS
+            url_pattern = re.compile(r'url\((\/[^)]+)\)')
+            matches = url_pattern.findall(content)
 
-        for old_url in matches:
-            new_url = format_media_link(url=old_url,
-                                        current_host=current_host,
-                                        site=user_site)
-            content = content.replace(f'url({old_url})', f'url({new_url})')
+            for old_url in matches:
+                new_url = format_media_link(url=old_url,
+                                            current_host=current_host,
+                                            site=user_site)
+                content = content.replace(f'url({old_url})', f'url({new_url})')
 
-        # Update used traffic based on content length
-        update_used_traffic(
-            traffic_amount=int(response.headers.get("Content-Length", 0)),
-            user_site=user_site
-        )
+            # Update used traffic based on content length
+            update_used_traffic(
+                traffic_amount=int(response.headers.get("Content-Length", 0)),
+                user_site=user_site
+            )
 
-        # Return modified CSS
-        proxy_response = HttpResponse(content, content_type=content_type)
-    else:
-        # Update used traffic for non-CSS resources
-        update_used_traffic(
-            traffic_amount=int(response.headers.get("Content-Length", 0)),
-            user_site=user_site
-        )
+            # Return modified CSS
+            proxy_response = HttpResponse(content, content_type=content_type)
+        else:
+            # Update used traffic for non-CSS resources
+            update_used_traffic(
+                traffic_amount=int(response.headers.get("Content-Length", 0)),
+                user_site=user_site
+            )
 
-        # Return non-CSS content directly
-        proxy_response = HttpResponse(response.content,
-                                      content_type=content_type)
+            # Return non-CSS content directly
+            proxy_response = HttpResponse(response.content,
+                                          content_type=content_type)
 
-    # Set CORS header
-    proxy_response["Access-Control-Allow-Origin"] = "*"
-    return proxy_response
+        # Set CORS header
+        proxy_response["Access-Control-Allow-Origin"] = "*"
+        return proxy_response
+    except requests.exceptions.RequestException:
+        return HttpResponseNotFound("The resource can not be downloaded")
